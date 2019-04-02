7b3f00e491af9f82ec8f27a4ae7d5f52156875c3
tdude92 <41596427+tdude92@users.noreply.github.com>
2019-04-02 19:47:25 -0400

    Update td_raytracer.py
    The light is now obstructed by the walls 👍

--- td_raytracer.py
+++ td_raytracer.py
@@ -1,4 +1,3 @@
-# In progress plz no roast
 import tkinter
 from math import sin, cos, radians
 
@@ -8,7 +7,7 @@ canvas = tkinter.Canvas(root, width = 400, height = 400)
 canvas.pack()
 
 # Draw the sides of the pillars.
-# Not rectangles so that we know with which side the light ray intersects.
+# Not rectangles because we want to know which side the rays intersects with.
 pillars = []
 y0 = 25
 for i in range(4):
@@ -31,20 +30,70 @@ lsy1 = 205
 lsc_x = (lsx0 + lsx1) / 2
 lsc_y = (lsy0 + lsy1) / 2
 
-light_source = canvas.create_oval(lsx0, lsy0, lsx1, lsy1, fill = "yellow")
-
 # Create 360 rays of light.
 # Use sin and cos to calculate x and y displacement from light source center to x1 and y1.
 rays = []
 for i in range(361):
-    x_displacement = 200 * cos(radians(i)) # adj = hyp*cos(x)
-    y_displacement = 200 * sin(radians(i)) # opp = hyp*sin(x)
+    x_displacement = 400 * cos(radians(i)) # adj = hyp*cos(x)
+    y_displacement = 400 * sin(radians(i)) # opp = hyp*sin(x)
     rays.append(canvas.create_line(lsc_x, lsc_y, lsc_x + x_displacement, lsc_y + y_displacement, fill = "yellow"))
 
 # Detect light rays that overlap with pillar sides and create a new line from the light source to the point of intersection (of the pillar side and light ray).
+counter = 0
 for i in pillars:
-    obstructed_rays = tkinter.find_overlapping(canvas.coords(i))
-    for i in obstructed_rays:
-        
+    counter += 1
+    obstructed_rays_with_imposters = list(canvas.find_overlapping(*canvas.coords(i)))
+
+    # Weed out imposter pillars.
+    obstructed_rays = [i for i in obstructed_rays_with_imposters if not i in pillars]
+
+    for j in obstructed_rays:
+        s_ep = canvas.coords(i) # Side endpoints.
+        r_ep = canvas.coords(j) # Ray endpoints.
+
+        try:
+            # Calculate slope of the lines.
+            s_m = (s_ep[1] - s_ep[3]) / (s_ep[0] - s_ep[2])
+            r_m = (r_ep[1] - r_ep[3]) / (r_ep[0] - r_ep[2])
+            # Calculate y-intercept of the lines.
+            s_b = s_ep[1] - s_m*s_ep[0]
+            r_b = r_ep[1] - r_m*r_ep[0]
+        except ZeroDivisionError:
+            # Account for vertical lines with undefined slopes.
+            if s_ep[0] - s_ep[2] == 0 and r_ep[0] - r_ep[2] == 0:
+                # The closer endpoint is the point of intersection.
+                if abs(s_ep[1] - lsc_y) < abs(s_ep[3] - lsc_y):
+                    poi_x = s_ep[0]
+                    poi_y = s_ep[1]
+                else:
+                    poi_x = s_ep[2]
+                    poi_y = s_ep[3]
+            elif s_ep[0] - s_ep[2] == 0:
+                # Calculate defined slope and y-intercept.
+                r_m = (r_ep[1] - r_ep[3]) / (r_ep[0] - r_ep[2])
+                r_b = r_ep[1] - r_m*r_ep[0]
+                # Calculate point of intersection if the side has an undefined slope.
+                poi_x = s_ep[0]
+                poi_y = r_m*poi_x + r_b
+            elif r_ep[0] - r_ep[2] == 0:
+                # Calculate defined slope and y-intercept.
+                s_m = (s_ep[1] - s_ep[3]) / (s_ep[0] - s_ep[2])
+                s_b = s_ep[1] - s_m*s_ep[0]
+                # Calculate point of intersection if the ray has an undefined slope.
+                poi_x = r_ep[0]
+                poi_y = s_m*poi_x + s_b
+            else:
+                raise ValueError
+        else:
+            # Calculate point of intersection x-value if slopes are not undefined.
+            poi_x = (s_b - r_b) / (r_m - s_m)
+            # Calculate point of intersection y-value if slopes are not undefined.
+            poi_y = r_m*poi_x + r_b
+
+        # Delete and replace line with new line.
+        canvas.delete(j)
+        rays.append(canvas.create_line(lsc_x, lsc_y, poi_x, poi_y, fill = "yellow"))
+
+light_source = canvas.create_oval(lsx0, lsy0, lsx1, lsy1, fill = "white", outline = "yellow")
 
 tkinter.mainloop()
